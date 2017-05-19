89f93442539223579a7630749e25eacc0b744f72
Tony <gybin02@live.cn>
2017-05-19 11:57:24 +0800

    Create IInjectCallback.java

---
+++ IInjectCallback.java
@@ -0,0 +1,13 @@
+package com.lingan.seeyou.ui.activity.user.controller.inject;
+
+import android.content.Context;
+
+import java.io.Serializable;
+
+/**
+ * @author zhengxiaobin@xiaoyouzi.com
+ * @since 16/11/11
+ */
+public interface IInjectCallback extends Serializable {
+    void call(Context context);
+}
